fa88607e8fc03afc3ad21dc7467eed631607f49c
weatherman223 <zjmiller2004@gmail.com>
2023-08-02 11:12:03 -0600

    Add length check to prevent trigger in middle of sentence

--- judgebot.py
+++ judgebot.py
@@ -10,19 +10,21 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
-        
-        if message.content.startswith('im ready') or message.content.startswith('Im ready') or "i'm ready" in message.content or "I'm ready" in message.content:
-            # Wait for 8 seconds before sending the message.
-            await asyncio.sleep(8)
+        # Keyword check
+        if message.content.lower('im ready') or message.content.lower('Im ready') or "i'm ready" in message.content.lower or "I'm ready" in message.content.lower:
+            # Checks if 7/8 characters. 
+            if len(message.content) in (7, 8):
+                # Wait for 8 seconds before sending the message.
+                await asyncio.sleep(8)
 
-            # Respond with "8 seconds."
-            await message.channel.send("8 seconds")
+                # Respond with "8 seconds."
+                await message.channel.send("8 seconds")
 
-            # Wait for 4 seconds before sending the second message.
-            await asyncio.sleep(4)
+                # Wait for 4 seconds before sending the second message.
+                await asyncio.sleep(4)
 
-            # Respond with "12 seconds."
-            await message.channel.send("12 seconds")
+                # Respond with "12 seconds."
+                await message.channel.send("12 seconds")
 
 intents = discord.Intents.default()
 intents.message_content = True
